7830337eaa97f3d031ce8a10d947d6b0748d5cb1
Christian HUANG <chrhuang@iphone-de-pa.home>
2021-09-24 18:44:09 +0200

    fix

---
+++ lib/interface.rb
@@ -0,0 +1,8 @@
+require_relative "croupier"
+
+# TODO: make the user play from terminal in a while loop that will stop
+#       when the user will not be asking for  a new card
+
+end
+
+puts end_game_message(player_score, bank_score)
